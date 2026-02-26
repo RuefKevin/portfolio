ae607d690d9d071c91021ed582a4df74861a7fc8
Kevin Ruef <kevin.ruef05@icloud.com>
2026-02-26 21:40:08 +0100

    fix: clean the terminal output from Svelte

--- frontend/src/routes/+page.server.ts
+++ frontend/src/routes/+page.server.ts
@@ -3,7 +3,6 @@ import type { PageServerLoad } from './$types';
 export const load: PageServerLoad = async () =>
 {
     const response = await fetch('http://127.0.0.1:3000/api/projects');
-    console.log('[${response.status}] GET /api/projects');
     const projects = await response.json();
     return { projects };
 };
