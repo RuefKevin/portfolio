90c5703c4a3ae78e3f8b05859a6ee53b6f0e31da
Kevin Ruef <kevin.ruef05@icloud.com>
2026-03-12 15:53:24 +0100

    chore: add new migration into database

---
+++ backend/migrations/20260312144909_seed_more_projects.sql
@@ -0,0 +1,3 @@
+INSERT INTO projects (name, description, technologies)
+VALUES ('Portfolio Frontend', 'Svelte/Typescript Frontend with performance focus', 'Svelte,Typescript,Javascript,CSS,HTML'),
+       ('RAW Converter', 'Python script to convert RAW-files into jpeg/png', 'Python');
